34617439197fcf930df069991f6cfca24b1fb5cf
seeratdhingra6 <118955100+seeratdhingra6@users.noreply.github.com>
2024-02-20 17:41:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const cookieParser = require("cookie-parser");
 const productRouter = require("./routes/product");
 const authRouter = require("./routes/auth");
 const cartRouter = require("./routes/cart");
+const dotenv = require("dotenv");
 
 const app = express();
-
+dotenv.config();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   })
 );
 app.use(express.json());
@@ -22,10 +23,8 @@ app.use("/product", productRouter);
 app.use("/auth", authRouter);
 app.use("/cart", cartRouter);
 
-mongoose.connect(
-  "mongodb+srv://ashishsingla18:B0QCe2fgl0q4chya@cluster0.7mrmiav.mongodb.net/?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGO_DB_URL);
 
-app.listen("3001", () => {
-  console.log("server is running on PORT 3001");
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on PORT ${process.env.PORT}`);
 });
